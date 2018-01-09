755a1c7cb47bd3c50395b2868a445d9d6cfde5cb
Paul McCarthy <paul.mccarthy@avant.com>
2018-01-08 18:42:12 -0600

    me

--- app/controllers/v1/pages_controller.rb
+++ app/controllers/v1/pages_controller.rb
@@ -1,5 +1,9 @@
 class V1::PagesController < ApplicationController
   def index
+<<<<<<< HEAD
     render json: {message: "Hello!", name: "Paul"}
+=======
+    render json: {message: "Hello!, Allen Cheng"}
+>>>>>>> ecba05c32fc1265525338eb453e478051abf78d0
   end
 end
