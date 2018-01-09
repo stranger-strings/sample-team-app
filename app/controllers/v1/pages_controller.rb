80bf73889c05f961be94d72773f04d094c16c304
Timothy Shores <timothymshores@gmail.com>
2018-01-08 18:37:40 -0600

    added my name to the json

--- app/controllers/v1/pages_controller.rb
+++ app/controllers/v1/pages_controller.rb
@@ -1,5 +1,5 @@
 class V1::PagesController < ApplicationController
   def index
-    render json: {message: "Hello!, Allen Cheng"}
+    render json: {message: "Hello!, Allen Cheng and also Tim"}
   end
 end
